1e0b9b47fe96d4b8cf6d263f91d8442e9ffce7fd
zakaria loulida <zakarialoulida92@gmail.com>
2024-02-22 16:56:50 +0100

    services updated

--- config/services.php
+++ config/services.php
@@ -35,5 +35,9 @@ return [
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => 'http://localhost/auth/google/call-back',
     ],
-
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => 'http://localhost/auth/facebook/call-back',
+    ],
 ];
